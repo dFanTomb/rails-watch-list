81adbfcfaf3078b9b37d7c1f5d607b445abf17d6
Darko Iliev <darkoilievsum@gmail.com>
2023-02-09 23:47:51 +0100

    not fully fucntioning

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,7 @@
 class List < ApplicationRecord
-  has_many :movies, dependent: :destroy # FIRST should destroy child saved movies when destroying self
-  has_many :bookmarks # ! SECOND ! first, list has many bookmaks
-  has_many :movies, through: 'bookmarks'  # ! THIRD ! and than list has many movies through bookmarks
+  has_many :bookmarks, dependent: :destroy
+  has_many :movies, through: :bookmarks, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,21 +3,24 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
   root to: 'lists#index'
-  post 'lists/:list_id/bookmarks', to: 'bookmarks#create', as: :list_bookmarks
-  get 'lists/:list_id/bookmarks/new', to: 'bookmarks#new', as: :new_list_bookmark
-  post 'lists/:list_id/reviews', to: 'reviews#create', as: :list_reviews
-  get 'lists', to: 'lists#index'
-  post 'lists', to: 'lists#create'
-  get 'lists/new', to: 'lists#new', as: :new_list
-  get 'lists/:id', to: 'lists#show'
-  delete 'lists/:id', to: 'lists#destroy'
-  delete 'bookmarks/:id', to: 'bookmarks#destroy', as: :bookmark
-  delete 'reviews/:id', to: 'reviews#destroy'
-  # resources :lists, except: [:edit, :update] do
-  #   resources :bookmarks, only: [:new, :create]
-  #   resources :reviews, only: :create
-  # end
-  # resources :bookmarks, only: :destroy
-  # resources :reviews, only: :destroy
+
+  # post 'lists/:list_id/bookmarks', to: 'bookmarks#create', as: :list_bookmarks
+  # get 'lists/:list_id/bookmarks/new', to: 'bookmarks#new', as: :new_list_bookmark
+  # post 'lists/:list_id/reviews', to: 'reviews#create', as: :list_reviews
+  # get 'lists', to: 'lists#index'
+  # post 'lists', to: 'lists#create'
+  # get 'lists/new', to: 'lists#new', as: :new_list
+  # get 'lists/:id', to: 'lists#show'
+  # delete 'lists/:id', to: 'lists#destroy'
+  # delete 'bookmarks/:id', to: 'bookmarks#destroy', as: :bookmark
+  # delete 'reviews/:id', to: 'reviews#destroy'
+
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create
+  end
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
